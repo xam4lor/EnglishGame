496e613fef28243dc1d14a24c46eb15b92cc1ca9
xam4lor <xam4lor@gmail.com>
2019-11-03 19:09:48 +0100

    Added game parametrization

--- routes/index.js
+++ routes/index.js
@@ -22,25 +22,53 @@ router
     // pages
     .get('/', (req, res) => {
         res.render('pages/accueil');
-
-        gameInstance.io.sockets.in(3).emit('message', 'Hi there!');
     })
 
     // game-logic
-    .get('/req/new_game', (req, res) => {
+    .get('/game/configuration', (req, res) => {
         let p = gameInstance.createParty();
         res.render('game/logic/create_game', { room_id: p.id });
     })
 
-    //.get('/game/config', (req, res) => res.render('game/logic/create_game'))
-    .get('/game/waiting', (req, res) => {
-        res.render('game/logic/waiting_screen');
+    // send game super-user config
+    .post('/req/set_config', (req, res) => {
+        let ans = gameInstance.handleConfig(req.body); // true if no errors
+
+        if(ans)
+            res.redirect(`/game/waiting?room_id=${req.body.room_id}`);
+        else
+            res.redirect('/game/configuration');
     })
+
+    // waiting for other players
+    .get('/game/waiting', (req, res) => {
+        let p = gameInstance.getParty(req.query.room_id);
+        if(!req.query.room_id || !p)
+            res.redirect('/');
+        else {
+            if(!p.began)
+                res.render('game/logic/waiting_screen', { room_id: req.query.room_id });
+            else
+                res.render(''); // redirect to party
+        }
+    })
+
+
     .get('/game/results', (req, res) => res.render('game/logic/results_game'))
 
     // game-playing
     .get('/game/answer', (req, res) => res.render('game/playing/answer_page'))
     .get('/game/results', (req, res) => res.render('game/playing/vote_page'))
+
+
+
+
+    .get('/debug', (req, res) => {
+        console.log(gameInstance.party_list);
+
+        if(gameInstance.party_list[0])
+            console.log(gameInstance.party_list[0].players);
+    })
 ;
 
 
--- routes/socket-io-comms.js
+++ routes/socket-io-comms.js
@@ -3,30 +3,71 @@ class GameParty {
         this.party_list = [];
         this.id_list    = [0];
         this.io         = io;
+
+        // To create a default void party for debugging
+        // this.createParty();
     }
 
-    // If already in a game, combine socketIp with globalIp
-    handleAdress(globalIp, socketIp) {
-        let found = false;
-        for (let i = 0; i < party_list.length; i++) {
-            if(party_list[i].players[globalIp]) {
-                party_list[i].players[globalIp].global_ip = globalIp;
-                party_list[i].players[globalIp].socket_ip = socket_ip;
-                found = true;
+
+
+    /**
+    * If already in a game, combine socketIp with globalIp
+    * @param globalIp Global ip
+    * @param socket Socket thread
+    */
+    handleAdress(globalIp, socket) {
+        let socketIp = socket.id;
+
+        for (let i = 0; i < this.party_list.length; i++) {
+            let p = this.party_list[i];
+
+            if(p.players[globalIp]) {
+                p.players[globalIp].global_ip = globalIp;
+                p.players[globalIp].socket_ip = socketIp;
+
+                socket.emit('redirect_user', '/game/waiting?room_id=' + p.id);
                 break;
             }
         }
     }
 
 
+    /**
+    * Joins a player to a specific party
+    * @param localIp User global ip
+    * @param socketId SocketIo ip
+    * @param roomId Id of the party
+    * @param isMainUser true if the user is the main user of the party
+    * @param socket the listening socket
+    */
+    joinPlayerTo(localIp, socketId, roomId, isMainUser, socket) {
+        let pa = this.getParty(roomId);
+        if(pa && pa.players[localIp] == undefined) {
+            pa.players[localIp] = {
+                global_ip: localIp,
+                socket_ip: socketId
+            };
+
+            if(isMainUser) pa.players[localIp].is_main_user = isMainUser;
+
+            socket.broadcast.to(roomId).emit('new_player', Object.keys(pa.players).length);
+        }
+
+        socket.join(roomId);
+    }
 
 
 
+
+
+
+    /**
+    * Creates a new party
+    */
     createParty() {
         let token = 0;
         while(this.id_list.includes(token)) {
-            //token = Math.round(Math.random() * 100000000);
-            token = 3;
+            token = Math.round(Math.random() * 100000000);
         }
         this.id_list.push(token);
 
@@ -34,13 +75,13 @@ class GameParty {
             id: "r_" + token,
             began: false,
             config: {
-
+                round_count : 5
             },
             players: {
-                // "TEMPLATE_IP": {
+                // "global_ip": {
                 //     is_main_user: false,
-                //     "global_ip" : "",
-                //     "socket_ip" : ""
+                //     global_ip : "",
+                //     socket_ip : ""
                 // }
             },
             game: {
@@ -53,7 +94,48 @@ class GameParty {
 
         return p;
     }
+
+
+
+    /**
+    * Check the config integrity and handle it
+    * @param b the req.body parameters
+    * @return true if values are OK and integrity is verified
+    */
+    handleConfig(b) {
+        if(!b.room_id || !b.round_count || isNaN(b.round_count)) {
+            this.party_list.pop();
+            this.id_list.pop();
+            return false;
+        }
+
+        let p = this.getParty(b.room_id);
+        if(!p) return false;
+
+        p.config.round_count = parseInt(b.round_count);
+
+        return true;
+    }
+
+
+
+
+
+
+    /**
+    * @param partyId the ID of the party
+    * @return a party or null if no party with that name found
+    */
+    getParty(partyId) {
+        for (let i = 0; i < this.party_list.length; i++)
+            if(this.party_list[i].id == partyId)
+                return this.party_list[i];
+
+        return null;
+    }
 }
+
+
 
 function runSocket(server) {
     let io = require('socket.io').listen(server);
@@ -63,11 +145,11 @@ function runSocket(server) {
     io.sockets.on('connection', function(socket) {
         // Handle local and socket ip if he isn't a "new" user
         let adress = socket.request.connection.remoteAddress;
-        gameInstance.handleAdress(adress, socket.id);
+        gameInstance.handleAdress(adress, socket);
 
 
-        socket.on('join_room', function(room) {
-            socket.join(room);
+        socket.on('join_room', function(room, isMainUser) {
+            gameInstance.joinPlayerTo(adress, socket.id, room, isMainUser, socket);
         });
     });
 
