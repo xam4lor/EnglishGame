44ccd3e114c8cce3c0fad4f4bf9721f00f546cd3
xam4lor <xam4lor@gmail.com>
2019-11-18 19:30:52 +0100

    Added TODO list

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ const PORT = process.env.PORT || 8080;
 
 /* ====== SERVER ====== */
 const server = app.listen(PORT, () => console.log(`Le serveur tourne sur le port ${ PORT } (@see 'localhost:${ PORT }')... Faites un effort quoi :/`));
+
+
+/**
+@TODO
+ - système pseudo
+ - temps  => temps de réaction moyen
+ - update des scores à chaque réponse
+ - système de logs
+*/
